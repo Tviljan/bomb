35fbc9116c197239a58f336e0e816e6d081c5b3f
Tviljan <virallinen@gmail.com>
2023-02-24 09:02:40 +0200

    added animation

--- src/nodes/bricks/breakable.gd
+++ src/nodes/bricks/breakable.gd
@@ -4,7 +4,10 @@ extends CSGBox3D
 signal on_after_explode
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	var tween = create_tween()
+	self.scale = Vector3.ZERO
+	var f =randf_range(0.2,0.5)
+	tween.tween_property(self, "scale", Vector3.ONE, f)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- src/nodes/pickable/bigger_explosion.gd
+++ src/nodes/pickable/bigger_explosion.gd
@@ -3,7 +3,9 @@ extends Node3D
 @onready var plus : CSGCombiner3D = $CSGCombiner3D
 # Called when the node enters the scene tree for the first time.
 func _ready(): # Replace with function body.
-	add_to_group("pickable")
+	var tween = create_tween()
+	self.scale = Vector3.ZERO
+	tween.tween_property(self, "scale", Vector3.ONE, 1)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
