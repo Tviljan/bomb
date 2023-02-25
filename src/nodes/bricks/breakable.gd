d5d43084431dc58edd712ba343e507f698022158
Tuomo Viljanmaa <virallinen@gmail.com>
2023-02-25 13:35:41 +0200

    Fix

--- src/nodes/bricks/breakable.gd
+++ src/nodes/bricks/breakable.gd
@@ -2,17 +2,21 @@ extends CSGBox3D
 
 
 signal on_after_explode
+var removed = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var tween = create_tween()
-	self.scale = Vector3.ZERO
 	var f =randf_range(0.2,0.5)
-	tween.tween_property(self, "scale", Vector3.ONE, f)
+	tween.tween_property(self, "scale", Vector3.ONE, f).from(Vector3.ZERO)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
+	
 func remove():
-	on_after_explode.emit(global_position)
-	queue_free()
+	if !removed:
+		removed = true
+		on_after_explode.emit(global_position)
+		var tween = create_tween()
+		tween.tween_property(self, "scale", Vector3.ZERO, 1)
+		tween.tween_callback(queue_free)
